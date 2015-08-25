baab2d31c76f4f5dbf9501879178ffba2c5f2f94
Kai Eichinger <cH40z-Lord@outlook.com>
2015-08-26 00:06:33 +0200

    Moved Wox.Plugin.Putty into its own folder

---
+++ Wox.Plugin.Putty/PuttySession.cs
@@ -0,0 +1,42 @@
+﻿namespace Wox.Plugin.Putty
+{
+    public class PuttySession
+    {
+        /// <summary>
+        /// The Putty Session connection identifier
+        /// </summary>
+        public string Identifier { get; set; }
+
+        /// <summary>
+        /// The Protocol that is used in the Putty Session
+        /// </summary>
+        public string Protocol { get; set; }
+
+        /// <summary>
+        /// The optional Username that is used in the Putty Session
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// The Hostname that is used in the Putty Session
+        /// </summary>
+        public string Hostname { get; set; }
+
+
+        /// <summary>
+        /// Returns a string that represents the current object.
+        /// </summary>
+        /// <returns>
+        /// A string that represents the current object.
+        /// </returns>
+        public override string ToString()
+        {
+            if (string.IsNullOrEmpty(Username))
+            {
+                return string.Format("{0}://{1}", Protocol, Hostname);
+            }
+
+            return string.Format("{0}://{1}@{2}", Protocol, Username, Hostname);
+        }
+    }
+}
---
+++ Wox.Plugin.Putty/PuttySessionService.cs
@@ -0,0 +1,46 @@
+﻿namespace Wox.Plugin.Putty
+{
+    using System.Collections.Generic;
+    using Microsoft.Win32;
+
+    public class PuttySessionService : IPuttySessionService
+    {
+        /// <summary>
+        /// Returns a List of all Putty Sessions
+        /// </summary>
+        /// <returns>A List of all Putty Sessions</returns>
+        public IEnumerable<PuttySession> GetAll()
+        {
+            var results = new List<PuttySession>();
+
+            using (var root = Registry.CurrentUser.OpenSubKey("Software\\SimonTatham\\PuTTY\\Sessions"))
+            {
+                if (root == null)
+                {
+                    return results;
+                }
+
+                foreach (var subKey in root.GetSubKeyNames())
+                {
+                    using (var puttySessionSubKey = root.OpenSubKey(subKey))
+                    {
+                        if (puttySessionSubKey == null)
+                        {
+                            continue;
+                        }
+
+                        results.Add(new PuttySession
+                        {
+                            Identifier = subKey,
+                            Protocol = puttySessionSubKey.GetValue("Protocol").ToString(),
+                            Username = puttySessionSubKey.GetValue("UserName").ToString(),
+                            Hostname = puttySessionSubKey.GetValue("HostName").ToString(),
+                        });
+                    }
+                }
+            }
+
+            return results;
+        } 
+    }
+}
